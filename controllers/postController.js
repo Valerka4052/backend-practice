928b2d12d56b6de9b884bb62e103f5377beaa34f
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-05-28 17:09:48 +0300

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -50,9 +50,9 @@ export const getAllPosts = async (req, res) => {
     
     const skip = (page - 1) * limit;
     try {
-        const postCount = await Post.find().length()
+        // const postCount = await Post.find().length()
         const allPosts = await Post.find().skip(skip).limit(limit).populate('user').exec();
-        res.status(200).json({ ...allPosts, contOfposts: postCount });
+        res.status(200).json(allPosts);
     } catch (error) {
         console.log(error);
         res.status(500).json('failure to get posts');
